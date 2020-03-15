e771f265e1d69c05a9de6c9a3c8d01d6ef8b29b5
Harsh Dubey <harsh.dubey@jacks.sdstate.edu>
2020-03-15 13:03:08 -0500

    Harsh Dubey

--- buggy.c
+++ buggy.c
@@ -10,28 +10,34 @@
 
 */
 
+/*
+Name: Harsh Dubey
+Changes: See comments
+Changes Made in CodeBlocks and then copied pasted in visual studio and then pushed
+*/
+
 struct dog
 {
 	int age;
-	char name = [10];
+	char name[10];//fixed the equal sign
 };
 
 
 void main()
 {
-	struct (dog) thing1:
+	struct dog thing1;//removed the parenthesis and colon
 	struct dog thing2;
 
-	thing1.age = [5];
+	thing1.age = 5;//removed the brackets
 	thing2.age = 13;
-	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
+
+	puts("What is your dogs name? ");//remove - {}
+	gets(thing1.name);//removed the "s" at the end
 
 	strcpy(thing2.name, "Buster");
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	printf("Thing 1 Name: %s\n", thing1.name);//programmed crashed - so change %i to %s  // Add Thing1 for better display
+	printf("Thing 1 Age: %i\n", thing1.age);//change colon to semi-colon                  // Add Thing1 for better display
+	printf("Thing2 Name: %s\n", thing2.name);//add - " //change position of - " to after \n
+	printf("Thing2 Age %i\n'", thing2.age);//add - " //change position of - " to after
 }
